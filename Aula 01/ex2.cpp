f8fd38a045c0b94465dded9ea8ccb4459204e175
hlanzieri <humbertolanzierifilho@gmail.com>
2026-03-26 19:31:26 -0300

    Refactoring the organization of the folder

---
+++ Aula 01/ex2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cout << "XX     XX XX    XX XXX     XXX XXXXXXXX XXXXXXXX XXXXXXX XXXXXXXXX XXXXXXXX" << endl;
+    cout << "XX     XX XX    XX XXXX   XXXX XX     X XX       XX    X     X     XX    XX" << endl;
+    cout << "XX     XX XX    XX XX XX XX XX XX     X XX       XX    X     X     X      X" << endl;
+    cout << "XXXXXXXXX XX    XX XX  XXX  XX XXXXXXX  XXXXXXXX XXXXXXX     X     X      X" << endl;
+    cout << "XX     XX XX    XX XX   X   XX XX     X XX       XXXX        X     X      X" << endl;
+    cout << "XX     XX XX    XX XX       XX XX     X XX       XX XXX      X     X      X" << endl;
+    cout << "XX     XX XX    XX XX       XX XX     X XX       XX  XXX     X     XX    XX" << endl;
+    cout << "XX     XX XXXXXXXX XX       XX XXXXXXXX XXXXXXXX XX   XX     X     XXXXXXXX" << endl;
+    return 0;
+}
